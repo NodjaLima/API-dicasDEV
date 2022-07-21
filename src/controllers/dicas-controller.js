7fe60dc0863e6473b57a477b7a0aee8ae64c1e0d
NodjaLima <nodjamplima@gmail.com>
2022-07-21 12:42:49 -0300

    criada rota post no arquivo dicas-controller.js

--- src/controllers/dicas-controller.js
+++ src/controllers/dicas-controller.js
@@ -1,6 +1,11 @@
 class DicasController {
-  rotas() {
-    app.get('/',() => {
+  rotas(app) {
+    app.get('/',(req, res) => {
+      res.send("<h1> Deu certo </h1>")
+    })
+
+    app.post('/',(req, res) => {
+      res.status(200).json({nome:'nodja'})
 
     })
   }
