8d47184a794b046bcc114817a9af11173ea549d0
NodjaLima <nodjamplima@gmail.com>
2022-07-21 23:57:34 -0300

    Insere metodos de validação nos verbos get e post no arquivo dicas-controller.js

--- src/controllers/dicas-controller.js
+++ src/controllers/dicas-controller.js
@@ -1,12 +1,26 @@
+import ValidaDicas from "../service/validacaoService.js"
+import DbMetodos from "../utils/db-metodos.js"
+
 class DicasController {
   rotas(app) {
-    app.get('/',(req, res) => {
-      res.send("<h1> Deu certo </h1>")
+    app.get('/', (req, resp) => {
+      const response = DbMetodos.retornoAleatorio()
+      resp.status(200).json(response)
     })
 
-    app.post('/',(req, res) => {
-      res.status(200).json({nome:'nodja'})
+    app.post('/', (req, resp) => {
+      const dicaValida = ValidaDicas.validaDica(req.body.dica);
+      const autorValido = ValidaDicas.validaAutor(req.body.autor);
 
+      if ((dicaValida) && (autorValido)) {
+        const novaDica = req.body
+        const response = DbMetodos.guardarDica(novaDica)
+        resp.status(200).json(response)
+      } else {
+        resp.status(200).json(Error)
+      }
+            
+      
     })
   }
 }
